6491d7cb80856b9f57d9643fe2c5358cbd852790
areif-dev <ajreifsnyder@protonmail.com>
2023-08-25 22:37:01 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,155 @@
+use river_layout_toolkit::{run, GeneratedLayout, Layout, Rectangle};
+use std::convert::Infallible;
+
+/// Create a Binary Space Partitioned layout. Specifically, this layout recursively
+/// divides the screen in half. The split will alternate between vertical and horizontal
+/// based on which side of the container is longer. This will result in a grid like
+/// layout with more-or-less equal sized windows even distributed across the screen
+///
+/// 3 Window Example:
+/// +---------+---------+
+/// |         |         |
+/// |         |         |
+/// |         |         |
+/// +         +---------+
+/// |         |         |
+/// |         |         |
+/// |         |         |
+/// +---------+---------+
+///
+/// 4 Window Example:
+/// +---------+---------+
+/// |         |         |
+/// |         |         |
+/// |         |         |
+/// +---------+---------+
+/// |         |         |
+/// |         |         |
+/// |         |         |
+/// +---------+---------+
+struct BSPLayout {}
+
+impl BSPLayout {
+    /// Perform the recursive division by two to evenly divide the screen as best
+    /// as possible
+    ///
+    /// # Arguments
+    ///
+    /// * `origin_x` - The x position of the top left of the space to be divided
+    /// relative to the entire display. For example, if you are dividing the entire
+    /// display, then the top left corner is 0, 0. If you are dividing the right
+    /// half of a 1920x1080 monitor, then the top left corner would be at 960, 0
+    ///
+    /// * `origin_y` - The y position of the top left of the space to be divided
+    /// relative to the entire display. For example, if you are dividing the entire
+    /// display, then the top left corner is 0, 0. If you are dividing the bottom
+    /// half of a 1920x1080 monitor, then the top left corner would be at 0, 540
+    ///
+    /// * `canvas_width` - The width in pixels of the area being divided. If you
+    /// are dividing all of a 1920x1080 monitor, then the `canvas_width` would be 1920.
+    /// If you are dividing the right half of the monitor, then the width is 960.
+    ///
+    /// * `canvas_height` - The height in pixels of the area being divided. If you
+    /// are dividing all of a 1920x1080 monitor, then the height would be 1080.
+    /// If you are dividing the bottom half of the monitor, then the height is 540.
+    ///
+    /// * `n_views` - How many windows / containers / apps / division the function
+    /// needs to make in total.
+    ///
+    /// # Returns
+    ///
+    /// A `GeneratedLayout` with `n_views` cells evenly distributed across the screen
+    /// in a grid
+    fn handle_layout_helper(
+        origin_x: u32,
+        origin_y: u32,
+        canvas_width: u32,
+        canvas_height: u32,
+        n_views: u32,
+    ) -> GeneratedLayout {
+        let mut layout = GeneratedLayout {
+            layout_name: "bsp-layout".to_string(),
+            views: Vec::with_capacity(n_views as usize),
+        };
+
+        // Exit condition. When there is only one window left, it should take up the
+        // entire available canvas
+        if n_views == 1 {
+            layout.views.push(Rectangle {
+                x: origin_x as i32,
+                y: origin_y as i32,
+                width: canvas_width,
+                height: canvas_height,
+            });
+
+            return layout;
+        }
+
+        let half_n_views = n_views / 2;
+        let views_remaining = n_views % 2; // In case there are odd number of views
+
+        let mut h1_width = canvas_width / 2 + canvas_width % 2;
+        let mut h2_width = canvas_width / 2;
+        let mut h1_height = canvas_height;
+        let mut h2_height = canvas_height;
+        let mut h2_x = h1_width + origin_x;
+        let mut h2_y = origin_y;
+
+        if canvas_height > canvas_width {
+            h1_width = canvas_width;
+            h2_width = canvas_width;
+            h1_height = canvas_height / 2 + canvas_height % 2;
+            h2_height = canvas_height / 2;
+            h2_x = origin_x;
+            h2_y = h1_height + origin_y;
+        }
+
+        let mut first_half =
+            Self::handle_layout_helper(origin_x, origin_y, h1_width, h1_height, half_n_views);
+
+        let mut sec_half = Self::handle_layout_helper(
+            h2_x,
+            h2_y,
+            h2_width,
+            h2_height,
+            half_n_views + views_remaining,
+        );
+
+        layout.views.append(&mut first_half.views);
+        layout.views.append(&mut sec_half.views);
+
+        layout
+    }
+}
+
+impl Layout for BSPLayout {
+    type Error = Infallible;
+
+    const NAMESPACE: &'static str = "bsp-layout";
+
+    fn user_cmd(
+        &mut self,
+        _cmd: String,
+        _tags: Option<u32>,
+        _output: &str,
+    ) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    fn generate_layout(
+        &mut self,
+        view_count: u32,
+        usable_width: u32,
+        usable_height: u32,
+        _tags: u32,
+        _output: &str,
+    ) -> Result<GeneratedLayout, Self::Error> {
+        let layout = Self::handle_layout_helper(0, 0, usable_width, usable_height, view_count);
+        Ok(layout)
+    }
+}
+
+fn main() {
+    let layout = BSPLayout {};
+    run(layout).unwrap();
+}
