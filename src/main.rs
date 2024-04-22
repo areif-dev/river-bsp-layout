18b708f992649a450fecca5198d410252a85d040
areif-dev <ajreifsnyder@protonmail.com>
2024-04-21 21:09:12 -0400

    Add individual cli args for each side of outer gap

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,29 @@ struct Cli {
     #[arg(short, long, default_value_t = 5)]
     inner_gap: u32,
 
-    /// The size of the gap between windows and the edge of the screen. In pixels
-    #[arg(short, long, default_value_t = 5)]
-    outer_gap: u32,
+    /// The default size of the gap between windows and the edge of the screen. In pixels
+    #[arg(short, long, default_value_t = 0)]
+    default_outer_gap: u32,
+
+    /// The number of pixels to place between the left screen edge and any windows. Overrides
+    /// `default_outer_gap` for the left side. Optional.
+    #[arg(long, short = 'l')]
+    og_left: Option<u32>,
+
+    /// The number of pixels to place between the right screen edge and any windows. Overrides
+    /// `default_outer_gap` for the right side. Optional.
+    #[arg(long, short = 'r')]
+    og_right: Option<u32>,
+
+    /// The number of pixels to place between the bottom screen edge and any windows. Overrides
+    /// `default_outer_gap` for the bottom side. Optional.
+    #[arg(long, short = 'b')]
+    og_bottom: Option<u32>,
+
+    /// The number of pixels to place between the top screen edge and any windows. Overrides
+    /// `default_outer_gap` for the top side. Optional.
+    #[arg(long, short = 't')]
+    og_top: Option<u32>,
 }
 
 fn main() {
