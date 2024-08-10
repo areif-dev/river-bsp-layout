6abe0272dea0ce25d9122f3949ca4a62ec68f5b6
areif-dev <ajreifsnyder@protonmail.com>
2024-08-10 16:33:35 -0400

    Add headings to help dialogue to make it easier to read

--- src/main.rs
+++ src/main.rs
@@ -9,75 +9,90 @@ use river_layout_toolkit::run;
 #[command(author, version, about, long_about = None)]
 struct Cli {
     /// The number of pixels to pad each inner edge of a window by default.
-    #[arg(short = 'i', long = "inner-gap", default_value_t = 0)]
+    #[arg(
+        short = 'i',
+        long = "inner-gap",
+        default_value_t = 0,
+        help_heading = "Inner Gap Options"
+    )]
     default_inner_gap: u32,
 
     /// The number of pixels to pad the left inner edge of each window. This Overrides
     /// `default_inner_gap`. Optional
-    #[arg(long, short = 'l')]
+    #[arg(long, short = 'l', help_heading = "Inner Gap Options")]
     ig_left: Option<u32>,
 
     /// The number of pixels to pad the right inner edge of each window. This Overrides
     /// `default_inner_gap`. Optional
-    #[arg(long, short = 'r')]
+    #[arg(long, short = 'r', help_heading = "Inner Gap Options")]
     ig_right: Option<u32>,
 
     /// The number of pixels to pad the bottom inner edge of each window. This Overrides
     /// `default_inner_gap`. Optional
-    #[arg(long, short = 'b')]
+    #[arg(long, short = 'b', help_heading = "Inner Gap Options")]
     ig_bottom: Option<u32>,
 
     /// The number of pixels to pad the top inner edge of each window. This Overrides
     /// `default_inner_gap`. Optional
-    #[arg(long, short = 't')]
+    #[arg(long, short = 't', help_heading = "Inner Gap Options")]
     ig_top: Option<u32>,
 
     /// The default size of the gap between windows and the edge of the screen.
-    #[arg(short = 'o', long = "outer-gap", default_value_t = 0)]
+    #[arg(
+        short = 'o',
+        long = "outer-gap",
+        default_value_t = 0,
+        help_heading = "Outer Gap Options"
+    )]
     default_outer_gap: u32,
 
     /// The number of pixels to place between the left screen edge and any windows. Overrides
     /// `default_outer_gap` for the left side. Optional.
-    #[arg(long, short = 'L')]
+    #[arg(long, short = 'L', help_heading = "Outer Gap Options")]
     og_left: Option<u32>,
 
     /// The number of pixels to place between the right screen edge and any windows. Overrides
     /// `default_outer_gap` for the right side. Optional.
-    #[arg(long, short = 'R')]
+    #[arg(long, short = 'R', help_heading = "Outer Gap Options")]
     og_right: Option<u32>,
 
     /// The number of pixels to place between the bottom screen edge and any windows. Overrides
     /// `default_outer_gap` for the bottom side. Optional.
-    #[arg(long, short = 'B')]
+    #[arg(long, short = 'B', help_heading = "Outer Gap Options")]
     og_bottom: Option<u32>,
 
     /// The number of pixels to place between the top screen edge and any windows. Overrides
     /// `default_outer_gap` for the top side. Optional.
-    #[arg(long, short = 'T')]
+    #[arg(long, short = 'T', help_heading = "Outer Gap Options")]
     og_top: Option<u32>,
 
     /// The default percentage of available area that the primary window should occupy after any
     /// split takes place.
-    #[arg(long = "split-perc", short = 's', default_value_t = 0.5)]
+    #[arg(
+        long = "split-perc",
+        short = 's',
+        default_value_t = 0.5,
+        help_heading = "Split Options"
+    )]
     default_split_perc: f32,
 
     /// The percentage of available area that the primary window should occupy after a horizontal
     /// split. This will override the value of `default_split_perc` only for horizontal splits.
-    #[arg(long, short = 'H')]
+    #[arg(long, short = 'H', help_heading = "Split Options")]
     hsplit_perc: Option<f32>,
 
     /// The percentage of available area that the primary window should occupy after a vertical
     /// split. This will override the value of `default_split_perc` only for vertical splits.
-    #[arg(long, short)]
+    #[arg(long, short, help_heading = "Split Options")]
     vsplit_perc: Option<f32>,
 
     /// Whether the first split that's made should divide the screen horizontally. If this is not
     /// set, then the first split will be vertical.
-    #[arg(long)]
+    #[arg(long, help_heading = "Split Options")]
     start_hsplit: bool,
 
     /// Reverse the order of the views as well as the order they are added.
-    #[arg(long)]
+    #[arg(long, help_heading = "Other Options")]
     reverse: bool,
 }
 
