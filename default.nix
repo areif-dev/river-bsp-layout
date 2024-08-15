d4f0c7742b4eb71cbe2853f2530679dfd5a24c72
areif-dev <ajreifsnyder@protonmail.com>
2024-08-15 17:52:32 -0400

    Update flake.lock and default.nix to use release v2.1.0

--- default.nix
+++ default.nix
@@ -1,13 +1,14 @@
 { fetchFromGitHub, rustPlatform, lib }:
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "river-bsp-layout";
   version = "2.1.0";
 
   src = fetchFromGitHub {
     owner = "areif-dev";
     repo = "river-bsp-layout";
-    rev = "f27d1d0d492c7e0022695e2a7a0fdf9343877d3d";
-    sha256 = "xsH5dyBx7UiIuttBkixjP+SXelyUI9feVncp574G8KU=";
+    rev = "v${version}";
+    sha256 = "sha256-LRVZPAS4V5PtrqyOkKUfrZuwLqPZbLoyjn2DPxCFE2o=";
   };
+
   cargoLock.lockFile = ./Cargo.lock;
 }
